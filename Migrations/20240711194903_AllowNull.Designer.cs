931f57e18dd59645c213ed2528e74e8da1dcddf9
Obasegun <obasootosegun@gmail.com>
2024-07-11 20:53:28 +0100

    working perfectly but require tweaking and page arrangement plus i have added docker

--- Migrations/20240711194903_AllowNull.Designer.cs
+++ Migrations/20240711194903_AllowNull.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TinyChat.Data;
 
@@ -11,9 +12,10 @@ using TinyChat.Data;
 namespace TinyChat.Migrations
 {
     [DbContext(typeof(ChatDbContext))]
-    partial class ChatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240711194903_AllowNull")]
+    partial class AllowNull
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
